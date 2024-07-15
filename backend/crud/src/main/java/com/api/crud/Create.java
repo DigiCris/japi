96815905e366de4d7746e5c6ed683205df10c29b
cmarchese <cmarchesetdiii@gmail.com>
2024-07-15 13:46:44 -0300

    el read ya funciona dentro de create cuando se llama por default al puerto

--- backend/crud/src/main/java/com/api/crud/Create.java
+++ backend/crud/src/main/java/com/api/crud/Create.java
@@ -8,6 +8,7 @@ import java.net.InetSocketAddress;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.google.gson.Gson;
@@ -92,7 +93,66 @@ public class Create {
 		}
 
 		private static String getAll() {
-			return "get";
+
+			try {
+				// Cargar el driver de MySQL
+				Class.forName("com.mysql.cj.jdbc.Driver");
+
+				// Conectarse a la base de datos
+				String url = "jdbc:mysql://localhost:3306/curs_flashivery";
+				String usuario = "root";
+				String contrasena = "";
+				Connection conexion = DriverManager.getConnection(url, usuario, contrasena);
+
+				// Crear una consulta SQL
+				String consulta = "SELECT *FROM `order`";
+				Statement declaracion = conexion.createStatement();
+				ResultSet resultado = declaracion.executeQuery(consulta);
+
+				// Crear un objeto JSON con los resultados
+				JsonArray ordenesJSON = new JsonArray();
+				while (resultado.next()) {
+					JsonObject ordenJSON = new JsonObject();
+					ordenJSON.addProperty("id", resultado.getInt("id"));
+					ordenJSON.addProperty("shop", resultado.getString("shop"));
+					ordenJSON.addProperty("shopId", resultado.getInt("shopId"));
+					ordenJSON.addProperty("order", resultado.getString("order"));
+					ordenJSON.addProperty("shipDate", resultado.getString("shipDate"));
+					ordenJSON.addProperty("Status", resultado.getString("Status"));
+					ordenJSON.addProperty("price", resultado.getString("price"));
+					ordenJSON.addProperty("pickAddress", resultado.getString("pickAddress"));
+					ordenJSON.addProperty("deliveryAddress", resultado.getString("deliveryAddress"));
+					ordenJSON.addProperty("quarrelDescription", resultado.getString("quarrelDescription"));
+					ordenJSON.addProperty("quarrelPicture", resultado.getString("quarrelPicture"));
+					ordenJSON.addProperty("reviewDescription", resultado.getString("reviewDescription"));
+					ordenJSON.addProperty("reviewLevel", resultado.getInt("reviewLevel"));
+					ordenJSON.addProperty("deliveryId", resultado.getInt("deliveryId"));
+					ordenJSON.addProperty("deliveryMoney", resultado.getString("deliveryMoney"));
+					ordenJSON.addProperty("userId", resultado.getInt("userId"));
+					ordenesJSON.add(ordenJSON);
+				}
+
+				JsonObject respuestaJSON = new JsonObject();
+				respuestaJSON.add("ordenes", ordenesJSON);
+
+				// Convertir el objeto JSON a una cadena JSON
+				Gson gson = new Gson();
+				String jsonString = gson.toJson(respuestaJSON);
+				String response = jsonString;
+				resultado.close();
+				declaracion.close();
+				conexion.close();
+
+				// Mostrar el JSON
+				return response;
+
+			} catch (ClassNotFoundException e) {
+				return ("Error al cargar el driver: " + e.getMessage());
+			} catch (SQLException e) {
+				return ("Error de SQL: " + e.getMessage());
+			}
+
 		}
+
 	}
 }
